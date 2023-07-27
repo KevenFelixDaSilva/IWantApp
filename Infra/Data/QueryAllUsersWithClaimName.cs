811e208325b14aae09081c69f27ef19b3a70e3e0
Keven Félix <keven.05@hotmail.com>
2023-07-27 07:39:11 -0300

    Created query | Add Dapper

---
+++ Infra/Data/QueryAllUsersWithClaimName.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using IWantApp.Endpoints;
+using IWantApp.Endpoints.Employees;
+using Microsoft.Data.SqlClient;
+
+namespace IWantApp.Infra.Data;
+
+public class QueryAllUsersWithClaimName
+{
+    private readonly IConfiguration configuration;
+
+    public QueryAllUsersWithClaimName(IConfiguration configuration)
+    {
+        this.configuration = configuration;
+    }
+
+    public IEnumerable<EmployeeResponse> Execute(int page, int rows)
+    {
+        var db = new SqlConnection(configuration["ConnectionString:IWantDb"]);
+        var query = 
+            @"SELECT Email, ClaimValue
+            FROM AspNetUsers u INNER 
+            JOIN AspNetUserClaims c
+            ON u.id = c.userId and claimtype = 'Name'
+            ORDER BY NAME
+            OFFSETS (@page -1)* @rows ROWS FETCH NEXT @rows ROWS ONLY";
+        return db.Query<EmployeeResponse>(
+            query,
+            new { page, rows }
+        );  
+    }
+}
